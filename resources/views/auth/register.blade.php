b5c7032733c8cf2de302d4ad4e701a599b395a20
prabhjot <prabhjots825@gmail.com>
2023-01-22 12:27:01 +0530

    changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,14 +25,14 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
-	<script src="scripts/bootstrap.min.html"></script>
+	<!-- <script src="scripts/bootstrap.min.html"></script> -->
     <script src="public/frontend/js/custom.js"></script>
 	
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 	<style>
         .card-style {
-            overflow: hidden;
+            /* overflow: hidden; */
             border-radius: 22px;
             margin: 0 5px 12px;
             border: none;
